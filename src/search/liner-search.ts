9285b4b3d46bda63aab6756c2832fc80fda51698
takuyaw-w <takuya.jamtb@gmail.com>
2021-07-13 20:54:33 +0900

    move

---
+++ src/search/liner-search.ts
@@ -0,0 +1,13 @@
+function linerSearch(array: number[], targetNumber: number): boolean {
+  for (let i = 0; array.length; i++) {
+    if (targetNumber === array[i]) {
+      console.log("hit", array[i])
+      return true;
+    }
+  }
+}
+
+const ary = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+const target = 10;
+const isCheck: boolean = linerSearch(ary, target);
+console.log(isCheck)
