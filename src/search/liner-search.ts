d6a32dbd4eeea27f32f6060aa28256d25461a1c2
takuyaw-w <takuya.jamtb@gmail.com>
2021-07-13 21:08:42 +0900

    update

--- src/search/liner-search.ts
+++ src/search/liner-search.ts
@@ -1,6 +1,11 @@
-function linerSearch(array: number[], targetNumber: number): boolean {
-  for (let i = 0; array.length; i++) {
-    if (targetNumber === array[i]) {
+interface data {
+  id: number;
+  data: string;
+}
+
+function linerSearch(array: data[], targetNumber: number): boolean {
+  for (let i = 0; i < array.length; i++) {
+    if (targetNumber === array[i].id) {
       console.log("hit", array[i])
       return true;
     }
@@ -8,7 +13,36 @@ function linerSearch(array: number[], targetNumber: number): boolean {
   return false;
 }
 
-const ary = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
-const target = 10;
+const ary: data[] = [
+  {
+    id: 9,
+    data: "data9"
+  },
+  {
+    id: 8,
+    data: "data8"
+  },
+  {
+    id: 101,
+    data: "data101"
+  },
+  {
+    id: 68,
+    data: "data68"
+  },
+  {
+    id: 47,
+    data: "data47"
+  },
+  {
+    id: 31,
+    data: "data31"
+  },
+  {
+    id: 91,
+    data: "data91"
+  },
+];
+const target = 101;
 const isCheck: boolean = linerSearch(ary, target);
 console.log(isCheck)
