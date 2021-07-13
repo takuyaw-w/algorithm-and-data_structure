23b44951bac2e53396e70c9631e906498608526a
takuyaw-w <takuya.jamtb@gmail.com>
2021-07-13 21:12:41 +0900

    deno fmt apply

--- src/search/liner-search.ts
+++ src/search/liner-search.ts
@@ -6,7 +6,7 @@ interface data {
 function linerSearch(array: data[], targetNumber: number): boolean {
   for (let i = 0; i < array.length; i++) {
     if (targetNumber === array[i].id) {
-      console.log("hit", array[i])
+      console.log("hit", array[i]);
       return true;
     }
   }
@@ -16,33 +16,33 @@ function linerSearch(array: data[], targetNumber: number): boolean {
 const ary: data[] = [
   {
     id: 9,
-    data: "data9"
+    data: "data9",
   },
   {
     id: 8,
-    data: "data8"
+    data: "data8",
   },
   {
     id: 101,
-    data: "data101"
+    data: "data101",
   },
   {
     id: 68,
-    data: "data68"
+    data: "data68",
   },
   {
     id: 47,
-    data: "data47"
+    data: "data47",
   },
   {
     id: 31,
-    data: "data31"
+    data: "data31",
   },
   {
     id: 91,
-    data: "data91"
+    data: "data91",
   },
 ];
 const target = 101;
 const isCheck: boolean = linerSearch(ary, target);
-console.log(isCheck)
+console.log(isCheck);
